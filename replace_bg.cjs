3d845ba7c02b8d1c4939799f83057b70cc32a14e
felixhencia04-crypto <felix.hencia04@gmail.com>
2026-08-08 05:33:38 +0700

    update

--- replace_bg.cjs
+++ replace_bg.cjs
@@ -1,18 +1,16 @@
 const fs = require('fs');
-
 let code = fs.readFileSync('src/pages/Kemitraan.tsx', 'utf8');
 
-// The line to replace:
-// style={{ backgroundImage: `url(${bgKemitraanHero})` }}
+// Add import
+if (!code.includes('import bgKemitraanHero')) {
+    code = code.replace(
+        "import { HEADER_BG_DATA } from '../assets/headerBgData';",
+        "import { HEADER_BG_DATA } from '../assets/headerBgData';\nimport bgKemitraanHero from '../assets/kemitraan-hero-bg.webp';"
+    );
+}
 
-// We want to remove bg-cover bg-center lg:bg-right-top bg-fixed from className
-code = code.replace(/bg-cover bg-center lg:bg-right-top bg-fixed /g, '');
-code = code.replace(/ bg-cover bg-center lg:bg-right-top bg-fixed/g, '');
-
-// We want to replace the style line and inject the img tag
-code = code.replace(
-  /style=\{\{ backgroundImage: `url\(\$\{bgKemitraanHero\}\)` \}\}\s*>\s*/g,
-  `>\n        <div className="absolute inset-0 w-full h-full pointer-events-none z-0">\n          <img src={bgKemitraanHero} alt="Background" className="w-full h-full object-cover object-center lg:object-right-top opacity-100" />\n        </div>\n        `
-);
+// Replace string paths with imported variable
+code = code.replace(/<img src="\/images\/bg-mitra\.png"/g, '<img src={bgKemitraanHero}');
+code = code.replace(/<img src="\/images\/bg-mitra\.webp"/g, '<img src={bgKemitraanHero}');
 
 fs.writeFileSync('src/pages/Kemitraan.tsx', code);
